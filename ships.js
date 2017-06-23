07d14c5d611bd9f880f31177e5e32b6db58b061e
Dominik Lipinski <maboonga@gmail.com>
2017-06-23 06:59:13 +0200

    main logic done

--- ships.js
+++ ships.js
@@ -8,17 +8,24 @@ var isSunk = false;
 
 while (isSunk==false) {
 	guess = prompt("Ready, steady, go - type number from 0 to 6: ");
-	if(guess!=location1&&guess!=location2&&guess!=location3){
+
+	if(guess<0||guess>6){
+		alert("Type correct number range 0 to 6: ");
+	} else {
 		guessesNumber = guessesNumber+1;
-		alert("Type correct number");
-	}
-	else if (guess==location1||guess!=location2||guess!=location3) {
+
+	if (guess==location1||guess==location2||guess==location3) {
 		hitsNumber=hitsNumber+1;
-		if(hitsNumber=3){
+		alert("Great Success! You've hit the enemy ship!");
+		
+		if(hitsNumber==3){
 			isSunk=true;
-			alert("Brawo, wygrałeś!");
+			alert("You won!");
 		}
 	}
-
+		else{
+			alert("You missed! Try again!");
+		}
+	}
 }
-alert("Wykonałeś "+guessesNumber+" prób");
+alert("Wykonałeś "+guessesNumber+" prób."+"Twoja efektywność to: "+(3/guessesNumber)+".");
